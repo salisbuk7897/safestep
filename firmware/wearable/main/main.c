12942a1971c6ba0ee8c82062ea3921321bed36bb
axl8192 <alejandrogarciagonzalezf@gmail.com>
2026-04-24 02:03:37 -0400

    Added Deep Sleep to wearable

--- firmware/wearable/main/main.c
+++ firmware/wearable/main/main.c
@@ -20,9 +20,13 @@
 #include "nimble/nimble_port.h"
 #include "nimble/nimble_port_freertos.h"
 #include "esp_nimble_hci.h"
+#include "esp_bt.h"
 
 /* NimBLE GAP APIs */
 #include "services/gap/ble_svc_gap.h"
+
+/* Deep Sleep */
+#include "esp_sleep.h"
 
 #define DEVICE_NAME "SafeStep-WB"
 uint8_t ble_addr_type;
@@ -30,15 +34,14 @@ uint8_t ble_addr_type;
 // The application
 void ble_app_on_sync(void){
     ble_hs_id_infer_auto(0, &ble_addr_type);
-    
+
     struct ble_hs_adv_fields adv_fields = {0};
     adv_fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_BREDR_UNSUP;
     adv_fields.name = (uint8_t *) ble_svc_gap_device_name();
     adv_fields.name_len = strlen((const char*)adv_fields.name);
     adv_fields.name_is_complete = 1;
-    adv_fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
-    adv_fields.tx_pwr_lvl_is_present = 1;
     ble_gap_adv_set_fields(&adv_fields);
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
 
     struct ble_gap_adv_params adv_params = {0};
     adv_params.itvl_max = 160;
@@ -48,13 +51,26 @@ void ble_app_on_sync(void){
     ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, NULL, NULL);
 }
 
+void timer_stop_nimble_port(TimerHandle_t xTimer){
+	nimble_port_stop();
+}
+
 void app_main(){
     nvs_flash_init();
-    nimble_port_init();   
+    nimble_port_init();
 
-    ble_svc_gap_init();                        
+    ble_svc_gap_init();
     ble_svc_gap_device_name_set(DEVICE_NAME);
-    
-    ble_hs_cfg.sync_cb = ble_app_on_sync;      
-    nimble_port_run();
+
+	TimerHandle_t t = xTimerCreate("timer_stop_nimble_port", pdMS_TO_TICKS(5000), pdFALSE, 0, timer_stop_nimble_port);
+	xTimerStart(t, 0);
+
+	ble_hs_cfg.sync_cb = ble_app_on_sync;
+	nimble_port_run();
+
+	// After nimble stops
+	nimble_port_deinit();
+	uint32_t wakeup_time_ms = 5000;
+	ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(wakeup_time_ms * 1000));
+	esp_deep_sleep_start();
 }
