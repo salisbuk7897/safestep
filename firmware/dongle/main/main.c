73278196966f1a0c5e8c0997e22a29a1acb4ae58
axl8192 <alejandrogarciagonzalezf@gmail.com>
2026-04-27 23:12:51 -0400

    Implemented configurable RSSI smoothing with moving averages

--- firmware/dongle/main/main.c
+++ firmware/dongle/main/main.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
+#include <stdlib.h>
+#include "freertos/idf_additions.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "esp_event.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
-#include "esp_nimble_hci.h"
 #include "nimble/nimble_port.h"
-#include "nimble/nimble_port_freertos.h"
 #include "host/ble_hs.h"
+#include "portmacro.h"
 #include "services/gap/ble_svc_gap.h"
-#include "sdkconfig.h"
 
 uint8_t ble_addr_type;
+#define SAMPLES_FOR_AVERAGE 10
+int rssi_samples[SAMPLES_FOR_AVERAGE] = {};
+int next_sample_index = 0;
+int taken_samples = 0; // max = SAMPLES_FOR_AVERAGE
+int cumulative_rssi = 0;
+
+SemaphoreHandle_t mutex;
 
 static int ble_gap_event(struct ble_gap_event *event, void *arg){
     struct ble_hs_adv_fields fields;
-    
+
     switch (event->type){
         case BLE_GAP_EVENT_DISC:
             ESP_LOGI("GAP", "GAP EVENT DISCOVERY");
             ble_hs_adv_parse_fields(&fields, event->disc.data, event->disc.length_data);
             if (fields.name_len > 0){
-                printf("Name: %.*s\n", fields.name_len, fields.name);
-                printf("RSSI: %d\n", event->disc.rssi);
+
+				xSemaphoreTake(mutex, portMAX_DELAY);
+				{
+					// Smooth RSSI with moving averages and send info to PC
+					cumulative_rssi -= rssi_samples[next_sample_index];
+					rssi_samples[next_sample_index] = event->disc.rssi;
+					cumulative_rssi += rssi_samples[next_sample_index];
+
+					next_sample_index = (next_sample_index+1)%SAMPLES_FOR_AVERAGE;
+
+					taken_samples = (taken_samples+1 < SAMPLES_FOR_AVERAGE)? taken_samples+1: SAMPLES_FOR_AVERAGE;
+
+					int smoothed_rssi = cumulative_rssi/taken_samples;
+					printf("Name: %.*s\n", fields.name_len, fields.name);
+					printf("RSSI: %d\n", smoothed_rssi);
+				}
+				xSemaphoreGive(mutex);
             }
             break;
         default:
@@ -34,7 +54,7 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg){
 
 void ble_app_on_sync(void){
     ble_hs_id_infer_auto(0, &ble_addr_type);
-    
+
     printf("Start scanning ...\n");
 
     struct ble_gap_disc_params disc_params;
@@ -49,12 +69,14 @@ void ble_app_on_sync(void){
 }
 
 void app_main(){
-    nvs_flash_init();
+	mutex = xSemaphoreCreateMutex();
+
+	nvs_flash_init();
     nimble_port_init();
 
     ble_svc_gap_init();
     ble_svc_gap_device_name_set("BLE-Scan-Client");
-                       
+
     ble_hs_cfg.sync_cb = ble_app_on_sync;
     nimble_port_run();
 }
